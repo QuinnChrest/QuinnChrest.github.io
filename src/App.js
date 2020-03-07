53e2794302f2fd845522304192060192e5f4f4f9
QuinnChrest <quinnchr@gmail.com>
2020-03-06 23:34:18 -0600

    added more pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import Home from './Components/Home';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/Projects" component={Projects}/>
+        <Route exact path="/Contact" component={Contact}/>
+        <Redirect from="/*" to="/"/>
       </Switch>
     </Router>
   );
