f2e7c7cc8c58e4e3445ec25da0c818e04d40b577
QuinnChrest <quinnchr@gmail.com>
2020-03-29 21:54:22 -0500

    new features
     - added react bootstrap containers
     - added nav bar
     - added logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
-import Home from './Components/Home';
-import Projects from './Components/Projects';
-import Contact from './Components/Contact';
+import {Container} from 'react-bootstrap';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+import Contact from './Pages/Contact';
+import NavigationBar from './Components/NavigationBar/NavigationBar';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/Projects" component={Projects}/>
-        <Route exact path="/Contact" component={Contact}/>
-        <Redirect from="/*" to="/"/>
-      </Switch>
-    </Router>
+    <React.Fragment>
+      <NavigationBar/>
+      <Container>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/Projects" component={Projects}/>
+            <Route exact path="/Contact" component={Contact}/>
+            <Redirect from="/*" to="/"/>
+          </Switch>
+        </Router>
+      </Container>
+    </React.Fragment>
   );
 }
 
---
+++ src/Components/NavigationBar/NavigationBar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, Image, Container } from 'react-bootstrap';
+import './NavigationBar.css';
+import logo from '../../Resources/logo.png';
+
+class Contact extends Component{
+    render(){
+        return (
+            <Navbar expand="lg">
+                <Container>
+                    <Navbar.Brand href="/"><Image className="logo" src={logo}/>Quinn Chrest</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link href="/Projects">Projects</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link href="/Contact">Contact</Nav.Link></Nav.Item>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        );
+    }
+}
+
+export default Contact;
