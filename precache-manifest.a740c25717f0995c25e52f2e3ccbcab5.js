a1407e2c0703965f176ee48bb5feb19e577084da
QuinnChrest <quinnchr@gmail.com>
2020-03-30 16:29:07 -0500

    Updates

--- precache-manifest.a740c25717f0995c25e52f2e3ccbcab5.js
+++ precache-manifest.a740c25717f0995c25e52f2e3ccbcab5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "095d974ea69b208300d66976e606d973",
+    "revision": "ad85f1531f0e10bae13053282bce7f5b",
     "url": "/index.html"
   },
   {
-    "revision": "155164dbf174f81bec70",
+    "revision": "87f0b4487107d6e7eaa2",
     "url": "/static/css/main.50394087.chunk.css"
   },
   {
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f95ffe9a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "155164dbf174f81bec70",
-    "url": "/static/js/main.50322bc6.chunk.js"
+    "revision": "87f0b4487107d6e7eaa2",
+    "url": "/static/js/main.e3d148ce.chunk.js"
   },
   {
     "revision": "281239441e02710dfb20",
     "url": "/static/js/runtime-main.23f92813.js"
   },
   {
-    "revision": "7069b08f4c20a62865819e8868162058",
-    "url": "/static/media/logo.7069b08f.png"
+    "revision": "ed3f0ecd5ec21b9ed12f2c132e5f6cf4",
+    "url": "/static/media/Logo.ed3f0ecd.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fcd3285beed449366739d03559e51dbd.js"
+  "/precache-manifest.a740c25717f0995c25e52f2e3ccbcab5.js"
 );
 
 self.addEventListener('message', (event) => {
